32af65daafba44d2dc27c7df3573bcfdd681536b
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-14 03:07:51 +0530

    converted to pulumi git repo

--- pkg/main.go
+++ pkg/main.go
@@ -3,21 +3,23 @@ package pkg
 import (
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/azurekeyvault"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/azure/pulumiazureprovider"
+	"github.com/pulumi/pulumi-azure/sdk/v5/go/azure"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-type ResourceStack struct {
-	Input  *azurekeyvault.AzureKeyVaultStackInput
-	Labels map[string]string
-}
-
-func (s *ResourceStack) Resources(ctx *pulumi.Context) error {
+func Resources(ctx *pulumi.Context, stackInput *azurekeyvault.AzureKeyVaultStackInput) error {
+	azureCredential := stackInput.AzureCredential
 	//create azure provider using the credentials from the input
-	_, err := pulumiazureprovider.Get(ctx, s.Input.AzureCredential)
+	_, err := azure.NewProvider(ctx,
+		"azure",
+		&azure.ProviderArgs{
+			ClientId:       pulumi.String(azureCredential.Spec.ClientId),
+			ClientSecret:   pulumi.String(azureCredential.Spec.ClientSecret),
+			SubscriptionId: pulumi.String(azureCredential.Spec.SubscriptionId),
+			TenantId:       pulumi.String(azureCredential.Spec.TenantId),
+		})
 	if err != nil {
 		return errors.Wrap(err, "failed to create azure provider")
 	}
-
 	return nil
 }
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,5 @@
 package outputs
 
-import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/azurekeyvault"
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+const (
+	OutputKey = "value"
 )
-
-func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
-	input *azurekeyvault.AzureKeyVaultStackInput) *azurekeyvault.AzureKeyVaultStackOutputs {
-	return &azurekeyvault.AzureKeyVaultStackOutputs{}
-}
