d0f370adf0c8dde593c877f74e939fcd8285be89
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-30 21:20:43 +0530

    using project-planton apis

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	azurekeyvaultv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/azure/azurekeyvault/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/azure-key-vault-pulumi-module/pkg"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/azurekeyvault"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		stackInput := &azurekeyvault.AzureKeyVaultStackInput{}
+		stackInput := &azurekeyvaultv1.AzureKeyVaultStackInput{}
 
 		if err := stackinput.LoadStackInput(ctx, stackInput); err != nil {
 			return errors.Wrap(err, "failed to load stack-input")
--- pkg/main.go
+++ pkg/main.go
@@ -1,13 +1,13 @@
 package pkg
 
 import (
+	azurekeyvaultv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/azure/azurekeyvault/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/azure/azurekeyvault"
 	"github.com/pulumi/pulumi-azure/sdk/v5/go/azure"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func Resources(ctx *pulumi.Context, stackInput *azurekeyvault.AzureKeyVaultStackInput) error {
+func Resources(ctx *pulumi.Context, stackInput *azurekeyvaultv1.AzureKeyVaultStackInput) error {
 	azureCredential := stackInput.AzureCredential
 	//create azure provider using the credentials from the input
 	_, err := azure.NewProvider(ctx,
